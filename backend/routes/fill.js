41997269aa293c776f4032df465d007c31036634
Péter Ott <ottpeter@tuta.io>
2022-09-04 10:01:14 +0200

    changed to 5000000

--- backend/routes/fill.js
+++ backend/routes/fill.js
@@ -45,7 +45,7 @@ router.get('/nfts_by_owner', async function(req, res) {
     request_type: "call_function",
     account_id: contractName,
     method_name: "nft_tokens",
-    args_base64: "eyJmcm9tX2luZGV4IjoiMCIsImxpbWl0Ijo0Mjk0OTY3Mjk2fQ==",
+    args_base64: "eyJmcm9tX2luZGV4IjoiMCIsImxpbWl0Ijo1MDAwMDAwfQ==",
     finality: "optimistic",
   });
 
